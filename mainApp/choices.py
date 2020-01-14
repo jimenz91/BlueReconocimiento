b890d66dcc3a5214387ef2c0837351862594f2b9
Carlos Jiménez <jimenz91@gmail.com>
2020-01-14 16:41:21 +0100

    Se agregaron comentarios descriptivos a choices.py para probar funcionalidad de branches de Git.

--- mainApp/choices.py
+++ mainApp/choices.py
@@ -1,6 +1,12 @@
 from mainApp.models import Categoria, Puntuacion, Proyecto
 from django.template.defaultfilters import lower, upper
 
+"""
+    Con esta lógica se obtienen los valores que hay en la base de datos para
+    categorías, proyectos y puntuaciones, de forma tal que se puedan comparar
+    luego cuando se generen nuevas menciones.
+"""
+
 n_categorias = Categoria.objects.all().values()
 n_puntuaciones = Puntuacion.objects.all().values()
 n_proyectos = Proyecto.objects.all().values()
