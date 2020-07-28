bd4d30dd3baff0960980abb598c64591dc9bdc4e
Carlos Jimenez | Banca <carlos.jimenez@bluetab.net>
2020-07-28 13:10:57 +0200

    Cambio de Sqlite a PostgreSql

--- BlueRecog/settings.py
+++ BlueRecog/settings.py
@@ -79,8 +79,16 @@ WSGI_APPLICATION = 'BlueRecog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bluerecog',
+
+        'USER': 'bluetab',
+
+        'PASSWORD': '9054BT.',
+
+        'HOST': 'localhost',
+
+        # 'PORT': '<db_port>',
     }
 }
 
